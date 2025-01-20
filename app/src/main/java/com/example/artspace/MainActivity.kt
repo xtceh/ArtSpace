2b61640eff8b4fa5c64dafb4c94e5fa98475f269
Ed <xtc_eh@hotmail.com>
2025-01-20 15:40:08 +0000

    Created front sheet layout with no User interaction

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,14 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
+import java.time.format.TextStyle
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +36,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ArtSpaceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    ArtSpaceLayout()
                 }
             }
         }
@@ -31,17 +45,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArtSpaceLayout(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.monalisa)
+    val imageDescription = stringResource(R.string.mona_lisa)
+    val imageTitle = stringResource(R.string.mona_lisa_title)
+    val imageArtist = stringResource(R.string.mona_lisa_artist)
+    val imageYear = stringResource(R.string.mona_lisa_year)
+
+    Column(modifier = Modifier.padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(painter = image, contentDescription = imageDescription)
+        }
+        //Spacer(modifier = Modifier.weight(1f))
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Column(modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = imageTitle, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                Text(text = "$imageArtist ($imageYear)", fontSize = 18.sp)
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Row(modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Button(onClick = {println("Previous Button Clicked!")},
+                modifier = Modifier.weight(1f)) {
+                Text(text = "Previous")
+            }
+            Button(onClick = {println("Next Button Clicked!")},
+                modifier = Modifier.weight(1f)) {
+                Text(text = "Next")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+/*@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtSpaceLayout("Android")
     }
-}
+}*/
